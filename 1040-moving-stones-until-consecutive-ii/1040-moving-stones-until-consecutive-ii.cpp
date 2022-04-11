55f6cdeaecd76002d780aa59cd09c7db14402dce
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-04-11 12:02:32 +0530

    Time: 26 ms (69.01%), Space: 13.2 MB (38.03%) - LeetHub

---
+++ 1040-moving-stones-until-consecutive-ii/1040-moving-stones-until-consecutive-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    vector<int> numMovesStonesII(vector<int>& stones) {
+        sort(stones.begin(),stones.end());
+        int n = stones.size();
+        int mini = n-1,i = 0, j = 0;
+        if(stones[n-1]-stones[0]==n-1)
+            mini=0;
+        else if((stones[n-2]-stones[0]==n-2 || stones[n-1]-stones[1]==n-2)){
+            mini = 2;
+            if(stones[1]-stones[0]==2 || stones[n-1]-stones[n-2]==2) mini=1;        
+        }else{
+            while(i<n){
+                if(stones[j]-stones[i]<=n-1){
+                    while(j<n && stones[j]-stones[i]<=n-1) 
+                        j++;
+                    mini = min(mini,n-(j-i));
+                }else{
+                    while(i<j && stones[j]-stones[i]>n-1) i++;
+                    mini = min(mini,n-(j-i));
+                }
+                i++;
+            }
+        }
+        //cout<<mini<<endl;
+        int maxi = 0;
+        for(int i=2;i<n-1;i++){
+            maxi+= max(0,stones[i]-stones[i-1]-1);
+        }
+        maxi+= max(0,max(stones[1]-stones[0]-1,stones[n-1]-stones[n-2]-1));
+        //cout<<maxi<<endl;
+        return{mini,maxi};
+    }
+};
