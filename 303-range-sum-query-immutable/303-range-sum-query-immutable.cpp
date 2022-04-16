8a7e0b323e97ee8040736c9933949a4ac6d89bdd
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-04-16 19:33:16 +0530

    Time: 27 ms (73.94%), Space: 17.2 MB (33.79%) - LeetHub

--- 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
+++ 303-range-sum-query-immutable/303-range-sum-query-immutable.cpp
@@ -1,20 +1,35 @@
 class NumArray {
 public:
-    vector<int>pre_sum;
-    NumArray(vector<int>& nums) {
-        int n = nums.size();
-        int sum = nums[0];
-        pre_sum.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-            sum+= nums[i];
-            pre_sum.push_back(sum);
+    vector<int>tree;
+    int n;
+    int constructST(vector<int>& nums, int l, int r, int idx){
+        if(l==r){
+            tree[idx] = nums[l];
+            return tree[idx];
         }
+        int m = (l+r)/2;
+        tree[idx] = constructST(nums,l,m,2*idx+1)+
+            constructST(nums,m+1,r,2*idx+2);
+        return tree[idx];
+    }
+    
+    NumArray(vector<int>& nums) {
+        n = nums.size();
+        tree.resize(4*n);
+        constructST(nums,0,n-1,0);
         return;
+    }
+    int getSum(int l, int r, int left, int right, int idx){
+        if(l>right || r<left){
+            return 0;
+        }
+        if(l<=left && r>=right) return tree[idx];
+        int m = (left+right)/2;
+        return getSum(l,r,left,m,2*idx+1)+getSum(l,r,m+1,right,2*idx+2);
     }
     
     int sumRange(int left, int right) {
-        if(left==0) return pre_sum[right];
-        return pre_sum[right]-pre_sum[left-1];
+        return getSum(left,right,0,n-1,0);
     }
 };
 
