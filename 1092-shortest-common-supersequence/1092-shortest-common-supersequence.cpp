26ab6c444d285591d56939df7e9e3b9a5a042d27
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-04-21 12:13:50 +0530

    Time: 13 ms (78.54%), Space: 13.2 MB (19.71%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -3,37 +3,24 @@ public:
     string shortestCommonSupersequence(string s1, string s2) {
         int n = s1.length();
         int m = s2.length();
-        vector<vector<pair<int,string>>>dp(n+1,vector<pair<int,string>>(m+1));
-        for(int i=0;i<=n;i++){dp[i][0].first=0; dp[i][0].second="";}
-        for(int j=0;j<=m;j++){dp[0][j].first=0; dp[0][j].second="";}
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++) dp[i][0] = 0;
+        for(int j=0;j<=m;j++) dp[0][j] = 0;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1]){
-                    dp[i][j].first = 1+dp[i-1][j-1].first;
-                    dp[i][j].second = dp[i-1][j-1].second+s1[i-1];
-                }else{
-                    if(dp[i-1][j].first>dp[i][j-1].first){
-                        dp[i][j].first = dp[i-1][j].first;
-                        dp[i][j].second = dp[i-1][j].second;
-                    }else{
-                        dp[i][j].first = dp[i][j-1].first;
-                        dp[i][j].second = dp[i][j-1].second;
-                    }
-                }
+                if(s1[i-1]==s2[j-1])
+                    dp[i][j] = 1+dp[i-1][j-1];
+                else
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
             }
         }
-        int siz = dp[n][m].first;
-        string s = dp[n][m].second;
         string ans = "";
-        int i=0,j=0,k=0;
-        while(k<siz){
-            while(s1[i]!= s[k]){ans+= s1[i];i++;}
-            while(s2[j]!=s[k]){ans+= s2[j];j++;}
-            ans+= s1[i];
-            i++;j++;k++;
+        int i=n,j=m;
+        while(i>0 || j>0){
+            if(i>0 && dp[i][j]==dp[i-1][j]){ans = s1[i-1]+ans; i--;}
+            else if(j>0 && dp[i][j]==dp[i][j-1]){ans = s2[j-1]+ans; j--;}
+            else{ans = s1[i-1]+ans; i--; j--;}
         }
-        while(i<n){ans+= s1[i]; i++;}
-        while(j<m){ans+= s2[j]; j++;}
         return ans;
     }
 };
