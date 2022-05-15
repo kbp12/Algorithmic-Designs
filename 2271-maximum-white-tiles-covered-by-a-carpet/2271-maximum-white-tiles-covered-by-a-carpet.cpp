368d0883b521e6bbface20f193dceae2282625ae
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-05-15 07:15:36 +0530

    Time: 903 ms (25.00%), Space: 69.7 MB (100.00%) - LeetHub

--- 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
+++ 2271-maximum-white-tiles-covered-by-a-carpet/2271-maximum-white-tiles-covered-by-a-carpet.cpp
@@ -1,52 +1,22 @@
 class Solution {
 public:
     int maximumWhiteTiles(vector<vector<int>>& tiles, int c) {
-        sort(tiles.begin(),tiles.end());
         int n = tiles.size();
-        // if(tiles[n-1][1]-tiles[0][0]+1<=c){
-        //     int ans = 0;
-        //     for(int i=0;i<n;i++) {
-        //         ans+= tiles[i][1]-tiles[i][0]+1;
-        //     }
-        //     return ans;
-        // }
-        int l = tiles[0][0];
-        int i =0;
-        int count = 0;
-        int j=0;
+        sort(tiles.begin(),tiles.end());
         int ans = 0;
-        //cout<<n<<endl;
-        while(i<n){
-            while(i<n && tiles[i][1]-l+1<=c){
-                count+= tiles[i][1]-tiles[i][0]+1;
-                i++;
-            }
-            if(i==n){
-                return max(count,ans);
-            }
-            int a=0;
-            if(i<n && tiles[i][0]-l+1<=c){
-                a = (l+c)-tiles[i][0];
-            }
-            count+=a;
-            //cout<<count<<" ";
-            ans = max(ans,count);
-            count-= a;
-            if(i<n){
-                count+= tiles[i][1]-tiles[i][0]+1;
-            }
-            while(j<i && tiles[i][1]-tiles[j][0]+1>=c ){
-                count-= (tiles[j][1]-tiles[j][0]+1);
+        int count = 0;
+        int j = 0;
+        for(int i=0;i<n;i++){
+            while(j<n && tiles[i][0]+c>tiles[j][1]){
+                count+= tiles[j][1]-tiles[j][0]+1;
                 j++;
             }
+            if(j!=n && tiles[i][0]+c>tiles[j][0]){
+                ans = max(ans,count+tiles[i][0]+c-tiles[j][0]);
+            }
             ans = max(ans,count);
-            if(j<n)
-                l = tiles[j][0];
-            //cout<<count<<" "<<endl;
-            i++;
+            count-= tiles[i][1]-tiles[i][0]+1;
         }
-        if(ans==8361) return 8371;
-        else if(ans==10702) return 10709;
         return ans;
     }
 };
