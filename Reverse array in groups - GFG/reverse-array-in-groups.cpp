4f84849fab2a27de1e1d3f075528f579e162e44e
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-05-30 08:47:02 +0530

    Added solution - LeetHub

--- Reverse array in groups - GFG/reverse-array-in-groups.cpp
+++ Reverse array in groups - GFG/reverse-array-in-groups.cpp
@@ -13,18 +13,9 @@ public:
     //Function to reverse every sub-array group of size k.
     void reverseInGroups(vector<long long>& arr, int n, int k){
         // code here
-        vector<long long>temp(n);
-        if(k==1) return;
-        int t = n/k;
-        for(int c=0;c<t;c++){
-            int l = k*c;
-            int r = k*(c+1)-1;
-            while(l<r){
-                swap(arr[l],arr[r]); l++; r--;
-            }
-        }
-        if(k*t<n){
-            int l = k*t, r = n-1;
+        for(int i=0;i<n;i+=k){
+            int l = i;
+            int r = min(i+k-1,n-1);
             while(l<r){
                 swap(arr[l],arr[r]); l++; r--;
             }
