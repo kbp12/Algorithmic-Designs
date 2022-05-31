b5e8ed1c718f660f718933fc740ffe8cd3cec738
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-05-31 21:00:59 +0530

    Time: 202 ms (37.45%), Space: 39.8 MB (81.55%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<vector<pair<int,int>>>adj(n);
+        vector<pair<int,int>>adj[n];
         for(int i=0;i<times.size();i++){
             adj[times[i][0]-1].push_back({times[i][1]-1,times[i][2]});
         }
-        vector<bool>visit(n,false);
-        vector<int>dist(n,1e9);
-        dist[k-1] = 0;
-        for(int x=0;x<n;x++){
-            int idx = -1, val = 1e9;
-            for(int i=0;i<n;i++){
-                if(visit[i]==false && (idx==-1 || dist[i]<val)){
-                    idx = i;
-                    val = dist[idx];
+        vector<bool>visited(n,false);
+        vector<int>time(n,1e9);
+        time[k-1] = 0;
+        for(int i=0;i<n;i++){
+            int idx = -1;
+            for(int j=0;j<n;j++){
+                if(visited[j]==false && (idx==-1 || time[j]<time[idx])){
+                    idx = j;
                 }
             }
-            if(idx==-1 || val==1e9) return -1;
-            visit[idx] = true;
-            for(int i=0;i<adj[idx].size();i++){
-                int a = adj[idx][i].first;
-                int b = adj[idx][i].second;
-                dist[a] = min(dist[a], dist[idx]+b);
+            if(idx==-1 || time[idx]==1e9) return -1;
+            visited[idx] = true;
+            for(int p=0;p<adj[idx].size();p++){
+                int a = adj[idx][p].first;
+                int b = adj[idx][p].second;
+                time[a] = min(time[a],time[idx]+b);
             }
         }
         int ans = -1;
         for(int i=0;i<n;i++){
-            if(visit[i]==false) return -1;
-            ans = max(ans,dist[i]);
+            if(visited[i]==false) return -1;
+            ans = max(ans,time[i]);
         }
         return ans;
     }
