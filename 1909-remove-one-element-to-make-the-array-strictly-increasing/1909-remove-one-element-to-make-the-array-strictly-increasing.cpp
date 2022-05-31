ad3ca3325a633055b0ccfbcd2492553e3ac095ec
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-05-31 09:01:35 +0530

    Time: 3 ms (92.89%), Space: 10 MB (60.72%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     bool canBeIncreasing(vector<int>& nums) {
-        int n = nums.size();
-        stack<int>st1,st2;
-        int i=0;
-        while(i<n){
-            if(st1.size()==0 || nums[st1.top()]<nums[i]){
-                st1.push(i); i++;
-            }else{
-                break;
+        int cnt = 0;
+        for (int i = 1; i < nums.size() && cnt < 2; ++i) {
+            if (nums[i - 1] >= nums[i]) {
+                ++cnt;
+                if (i > 1 && nums[i - 2] >= nums[i])
+                    nums[i] = nums[i - 1];
             }
         }
-        i = n-1;
-        while(i>=0){
-            if(st2.size()==0 || nums[st2.top()]>nums[i]){
-                st2.push(i); i--;
-            }else{
-                break;
-            }
-        } 
-        int a = st1.top(), b = st2.top();
-        if(a>b) return true;
-        else if(a==b-1){
-            if(st1.size()==1 || st2.size()==1) return true;
-            st1.pop(); st2.pop();
-            int c = st1.top(), d = st2.top();
-            if(nums[b]>nums[c]) return true;
-            else if(nums[d]>nums[a]) return true;
-            return false;
-        }
+        if(cnt<2) return true;
         return false;
     }
 };
