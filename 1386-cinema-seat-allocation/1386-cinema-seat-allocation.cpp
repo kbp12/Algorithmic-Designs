58a213c4709a9d866c6e26e00c3d6456df33a70a
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-06-02 23:05:39 +0530

    Time: 397 ms (17.58%), Space: 49.6 MB (98.31%) - LeetHub

---
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxNumberOfFamilies(int n, vector<vector<int>>& r) {
+        int m = r.size();
+        int ans = 0;
+        int prev = 0;
+        sort(r.begin(),r.end());
+        int c = 0;
+        for(int i=0;i<m;i++){
+            if(prev==r[i][0]){
+                if(r[i-1][1]==1 && r[i][1]==10) ans+=2;
+                else if(r[i-1][1]<=3 && r[i][1]>=8) ans++;
+                else if(r[i-1][1]==1 && r[i][1]>=6) ans++;
+                else if(r[i][1]==10 && r[i-1][1]<=5) ans++;
+            }else{
+                c++;
+                if(prev){
+                    if(r[i-1][1]==1) {ans+=2;}
+                    else if(r[i-1][1]<=5) {ans++;}
+                }
+                if(r[i][1]==10)
+                    ans+=2;
+                else if(r[i][1]>=6)
+                    ans++;
+                prev = r[i][0];
+            }
+            cout<<ans<<" ";
+        }
+        if(r[m-1][1]==1) ans+=2;
+        else if(r[m-1][1]<=5) ans++;
+        c = n-c;
+        ans+= 2*c;
+        return ans;
+    }
+};
