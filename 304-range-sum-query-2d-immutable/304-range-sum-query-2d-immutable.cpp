7954b88cd05ad690a5cc68f60723035af5d2ec6e
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-06-03 07:19:05 +0530

    Time: 427 ms (93.52%), Space: 148.1 MB (48.25%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -2,21 +2,21 @@ class NumMatrix {
 public:
     vector<vector<int>>dp;
     NumMatrix(vector<vector<int>>& matrix) {
-        int m =  matrix.size();
+        int m = matrix.size();
         int n = matrix[0].size();
         vector<int>v(n+1,0);
         dp.push_back(v);
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
-                v[j] = matrix[i-1][j-1]+v[j-1]+dp[i-1][j]-dp[i-1][j-1];
+                v[j] = matrix[i-1][j-1]-dp[i-1][j-1]+dp[i-1][j]+v[j-1];
             }
             dp.push_back(v);
-        } 
+        }
     }
     
     int sumRegion(int r1, int c1, int r2, int c2) {
-        r2++; c2++;
-        return dp[r2][c2]-dp[r1][c2]-dp[r2][c1]+dp[r1][c1];
+        r2++, c2++;
+        return dp[r2][c2]+dp[r1][c1]-dp[r1][c2]-dp[r2][c1];
     }
 };
 
