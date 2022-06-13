d784aed9711fc0f9695543b4e8bb8f41f942e290
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-06-13 10:15:07 +0530

    Time: 403 ms (8.39%), Space: 14.9 MB (46.85%) - LeetHub

--- 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.py
+++ 2063-vowels-of-all-substrings/2063-vowels-of-all-substrings.py
@@ -1,18 +1,12 @@
-class Solution {
-public:
-    long long countVowels(string w) {
-        int n = w.size();
-        vector<long long>dp(n+1,0);
-        long long ans = 0;
-        for(int i=1;i<=n;i++){
-            char c = w[i-1];
-            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'){
-                dp[i] = dp[i-1]+i;
-            }else{
-                dp[i] = dp[i-1];
-            }
-            ans+=dp[i];
-        }
-        return ans;
-    }
-};
+class Solution:
+    def countVowels(self, w: str) -> int:
+        n = len(w)
+        ans = 0
+        prev = 0
+        for i in range(n):
+            if(w[i]=='a' or w[i]=='e' or w[i]=='i' or w[i]=='o' or w[i]=='u'):
+                prev+=i+1
+                ans+=prev
+            else:
+                ans+=prev
+        return ans
