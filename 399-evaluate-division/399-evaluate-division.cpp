9f92e50c97f395fb7230b0ac51a8d953fcf9702d
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-06-16 19:27:11 +0530

    Time: 3 ms (67.86%), Space: 8.3 MB (75.96%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-        double search(string s,string e,unordered_set<string>& visited,unordered_map<string,unordered_map<string,double>>& mpp){
-        if(mpp.find(s)==mpp.end())
-            return -1.0;
-            
-        if(mpp[s].find(e)!=mpp[s].end())
-            return mpp[s][e];
-        visited.insert(s);
-            
-        for(auto it:mpp[s])
-        {
-            if(visited.find(it.first)==visited.end())
-            {
-                double res = search(it.first,e,visited,mpp);
-                if(res != -1.0){
-                    res = res*it.second;
+    double bfs(string s, string e, unordered_set<string>&visit, unordered_map<string,unordered_map<string,double>>&mp){
+        if(mp[s].size()==0) return -1.0;
+        if(mp[s].find(e)!=mp[s].end()){
+            return mp[s][e];
+        }
+        visit.insert(s);
+        for(auto it:mp[s]){
+            if(visit.find(it.first)==visit.end()){
+                double res = bfs(it.first,e,visit,mp);
+                if(res!=-1){
+                    res *= it.second;
                     return res;
                 }
-            }       
+            }
         }
         return -1.0;
     }
     
-    vector<double> calcEquation(vector<vector<string>>& eq, vector<double>& vals, vector<vector<string>>& q) {
-        unordered_map<string,unordered_map<string,double>> mpp;
-        
-        for(int i=0;i<eq.size();i++){
-            string f = eq[i][0];
-            string t = eq[i][1];
-            mpp[f].insert({t,vals[i]});
-            double temp = 1/vals[i];
-            mpp[t].insert({f,temp});
+    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
+        unordered_map<string,unordered_map<string,double>>mp;
+        for(int i=0;i<equations.size();i++){
+            mp[equations[i][0]].insert({equations[i][1],values[i]});
+            double d = (double)1/values[i];
+            mp[equations[i][1]].insert({equations[i][0],d});
         }
-        
-        vector<double> ans(q.size());
-        for(int i=0;i<q.size();i++)
-        {
-            unordered_set<string> visited;
-            ans[i] = search(q[i][0],q[i][1],visited,mpp);
+        vector<double>ans(queries.size());
+        for(int i=0;i<queries.size();i++){
+            unordered_set<string>visit;
+            ans[i] = bfs(queries[i][0],queries[i][1],visit,mp);
         }
-        return ans;   
-    }    
+        return ans;
+    }
 };
