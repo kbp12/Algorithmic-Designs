305b92cb94a7267a60e8c6e4ac66eee987b8fc2a
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-06-21 07:22:07 +0530

    Time: 98 ms (33.03%), Space: 7.4 MB (65.85%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int find(vector<int>& nums, int i, int j, int sum){
-        if(i==j) return sum;
+    int find(vector<int>& nums, int i, int j){
+        if(i==j) return nums[i];
         if(i==j-1) return max(nums[i],nums[j]);
-        return max(sum-find(nums,i+1,j,sum-nums[i]),sum-find(nums,i,j-1,sum-nums[j]));
+        return max(nums[i]+min(find(nums,i+2,j),find(nums,i+1,j-1)),nums[j]+min(find(nums,i+1,j-1),find(nums,i,j-2)));
     }   
     
     bool PredictTheWinner(vector<int>& nums) {
         int sum = 0;
         for(auto num:nums) sum+=num;
         int i=0, j=nums.size()-1;
-        int ans = find(nums,i,j,sum);
+        int ans = find(nums,i,j);
         if(ans>=(sum-ans)) return true;
         return false;
     }
