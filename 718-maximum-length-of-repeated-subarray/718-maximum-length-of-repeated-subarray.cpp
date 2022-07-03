82f301196ad7e553afccf48df5bacdcd5dd0bcf6
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-03 17:33:17 +0530

    Time: 619 ms (20.29%), Space: 109.1 MB (33.43%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -7,8 +7,6 @@ public:
             for(int j=1;j<=m;j++){
                 if(nums1[i-1]==nums2[j-1]){
                     dp[i][j] = 1+dp[i-1][j-1];
-                }else{
-                    dp[i][j] = 0;
                 }
                 ans = max(ans,dp[i][j]);
             }
