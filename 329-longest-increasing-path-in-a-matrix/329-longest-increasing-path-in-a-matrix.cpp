7bb8b78404429bcf2d0092a8282ab7c3e6dd78b9
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-03 13:32:43 +0530

    Time: 49 ms (88.80%), Space: 16.1 MB (50.90%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -12,7 +12,7 @@ public:
         if(j<n-1 && matrix[i][j+1]>matrix[i][j])
             d+=dis(matrix,i,j+1,m,n,dp);
         
-        dp[i][j] = max(a,max(b,max(c,d)));
+        dp[i][j] = max({a,b,c,d});
         return dp[i][j];
     }
     
@@ -21,9 +21,7 @@ public:
         int n = matrix[0].size();
         int c = 0;
         int ans = 0;
-        
         vector<vector<int>>dp(m,vector<int>(n,0));
-        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 c = dis(matrix,i,j,m,n,dp);
