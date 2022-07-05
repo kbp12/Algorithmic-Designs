b3d97d33a2ab56ec4daea6f76be033e8e5314d97
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-05 13:03:09 +0530

    Time: 259 ms (19.14%), Space: 40.5 MB (75.45%) - LeetHub

---
+++ 1702-maximum-binary-string-after-change/1702-maximum-binary-string-after-change.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string maximumBinaryString(string s) {
+        int n = s.length();
+        int c=0;
+        int last = -1;
+        for(int i=0;i<n;i++){
+            if(s[i]=='0'){
+                c++; last = i;
+            }
+        }
+        int i=0;
+        while(i<n && s[i]=='1'){
+               i++;
+        }
+        for(int j=1;j<c && i<n;j++){
+            s[i] = '1'; i++;
+        }
+        s[i] = '0'; i++;
+        while(i<n){
+            s[i] = '1'; i++;
+        }
+        return s;
+    }
+};
