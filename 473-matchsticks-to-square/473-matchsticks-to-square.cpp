e27c212b0c5f4f3e44155844f4f7f0d64fe27a06
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-12 11:38:23 +0530

    Time: 362 ms (52.12%), Space: 10.1 MB (47.49%) - LeetHub

--- 473-matchsticks-to-square/473-matchsticks-to-square.cpp
+++ 473-matchsticks-to-square/473-matchsticks-to-square.cpp
@@ -3,14 +3,14 @@ public:
     int s;
     vector<int>dp;
     int a[15];
-    bool backtrack(int i, int n, int a[]){
+    bool backtrack(int i, int n){
         if(i==n){
             return true;
         }
         for(int j=0;j<4;j++){
             if(dp[j]+a[i]<=s){
                 dp[j]+=a[i];
-                if(backtrack(i+1,n,a)){
+                if(backtrack(i+1,n)){
                     return true;
                 }
                 dp[j]-=a[i];
@@ -30,6 +30,6 @@ public:
         if(s%4) return false;
         s = s/4;
         dp.assign(4,0);
-        return backtrack(0,n,a);
+        return backtrack(0,n);
     }
 };
