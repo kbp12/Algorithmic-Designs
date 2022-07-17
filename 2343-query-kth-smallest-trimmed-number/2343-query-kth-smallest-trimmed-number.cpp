3ab4f52c47857cbde29025b989f2ad6ae8f901ce
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-17 16:17:36 +0530

    Time: 1905 ms (11.11%), Space: 239.5 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -1,35 +1,18 @@
 class Solution {
 public:
-    
-    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& q) {
-        int n = nums.size(); 
-        int m = q.size();
-        vector<int>ans(m,0);
-        
-        for(int i=0;i<m;i++){  
-            vector<pair<int , string >>vec(n);
-            for(int j=0;j<n;j++){
-                vec[j] = {j,nums[j]};
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        int n = nums.size();
+        int m = queries.size();
+        vector<int>ans(m);
+        for(int k=0;k<m;k++){
+            int f = queries[k][0], s = queries[k][1];
+            vector<pair<string,int>>temp(n);
+            for(int i=0;i<n;i++){
+                int siz = nums[i].size();
+                temp[i] = {nums[i].substr(siz-s), i};
             }
-            int firstt = q[i][0], secondd = q[i][1];
-            
-            for(int j=0;j<n;j++){
-                int siz = vec[j].second.size(); 
-                int idx = siz-secondd; 
-                while(idx<siz-1 && vec[j].second[idx]=='0') idx++;
-                    vec[j].second = vec[j].second.substr(idx);
-            }
-            sort(vec.begin(),vec.end(),
-                [](pair<int,string>&p1, pair<int,string>&p2){
-                        int siz1 = p1.second.size();
-                        int siz2 = p2.second.size();
-                        if(siz1!=siz2) return siz1<siz2;
-                        if(p1.second!=p2.second) 
-                            return p1.second<p2.second;
-                        return p1.first<p2.first;
-                });
-            ans[i] = vec[firstt-1].first;
-            vec.clear();
+            sort(temp.begin(),temp.end());
+            ans[k] = temp[f-1].second;
         }
         return ans;
     }
