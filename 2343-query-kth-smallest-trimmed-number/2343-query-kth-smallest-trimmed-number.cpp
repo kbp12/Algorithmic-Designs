9344280a63bc5a61db75bac7ef1d113252fa3915
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-17 09:54:59 +0530

    Time: 1773 ms (11.11%), Space: 466.3 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -4,31 +4,33 @@ public:
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& q) {
         int n = nums.size(); 
         int m = q.size();
-        vector<int>res(m,0);
+        vector<int>ans(m,0);
+        
         for(int i=0;i<m;i++){  
-            vector<pair<string,int>>v(n);
+            vector<pair<int , string >>vec(n);
             for(int j=0;j<n;j++){
-                v[j] = {nums[j],j};
+                vec[j] = {j,nums[j]};
             }
-            int f = q[i][0], s = q[i][1];
+            int firstt = q[i][0], secondd = q[i][1];
+            
             for(int j=0;j<n;j++){
-                int siz = v[j].first.size(); 
-                int st = siz-s; 
-                while(st<siz-1 && v[j].first[st]=='0') st++;
-                    v[j].first = v[j].first.substr(st);
+                int siz = vec[j].second.size(); 
+                int idx = siz-secondd; 
+                while(idx<siz-1 && vec[j].second[idx]=='0') idx++;
+                    vec[j].second = vec[j].second.substr(idx);
             }
-            sort(v.begin(),v.end(),
-                [](pair<string,int>&a, pair<string,int>&b){
-                        int c = a.first.size();
-                        int d = b.first.size();
-                        if(c!=d) return c<d;
-                        if(a.first!=b.first) 
-                            return a.first<b.first;
-                        return a.second<b.second;
+            sort(vec.begin(),vec.end(),
+                [](pair<int,string>&p1, pair<int,string>&p2){
+                        int siz1 = p1.second.size();
+                        int siz2 = p2.second.size();
+                        if(siz1!=siz2) return siz1<siz2;
+                        if(p1.second!=p2.second) 
+                            return p1.second<p2.second;
+                        return p1.first<p2.first;
                 });
-            res[i] = v[f-1].second;
-            v.clear();
+            ans[i] = vec[firstt-1].first;
+            vec.clear();
         }
-        return res;
+        return ans;
     }
 };
