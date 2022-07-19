4475bc8a4555ac8201ae5ebcd33f2e4f37d868d6
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-19 22:46:36 +0530

    Time: 898 ms (16.67%), Space: 0B (100.00%) - LeetHub

--- 584-find-customer-referee/584-find-customer-referee.sql
+++ 584-find-customer-referee/584-find-customer-referee.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
 SELECT Customer.name
 FROM Customer
-WHERE Customer.id NOT IN 
-    (
-        SELECT Customer.id
-        FROM Customer
-        WHERE Customer.referee_id = 2
-    )
+WHERE Customer.referee_id <>2 OR Customer.referee_id IS NULL
 ;
