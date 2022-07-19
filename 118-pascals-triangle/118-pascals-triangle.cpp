7f251a1999d0f3b0495e498762cbf300c8d77123
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-19 09:17:53 +0530

    Time: 3 ms (38.93%), Space: 6.6 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int n) {
+        vector<vector<int>>ans(n);
+        ans[0].push_back(1);
+        for(int i=1;i<n;i++){
+            int siz = ans[i-1].size();
+            ans[i].push_back(1);
+            for(int j=1;j<siz;j++){
+                ans[i].push_back(ans[i-1][j-1]+ans[i-1][j]);
+            }
+            ans[i].push_back(1);
+        }
+        return ans;
+    }
+};
