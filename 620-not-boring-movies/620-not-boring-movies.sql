dd4b45273a794f622ea914631bccfe825100de2a
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-20 23:25:07 +0530

    Time: 658 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 620-not-boring-movies/620-not-boring-movies.sql
+++ 620-not-boring-movies/620-not-boring-movies.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
 SELECT Cinema.*
 FROM Cinema
-WHERE Cinema.description <> 'boring' AND MOD(Cinema.id,2) = 1
+WHERE Cinema.description != 'boring' AND MOD(Cinema.id,2) = 1
 ORDER BY Cinema.rating DESC
 ;
