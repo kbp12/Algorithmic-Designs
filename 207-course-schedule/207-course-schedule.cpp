f9456728fff633c80477fb1e1cf9da384959ed7c
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-30 09:23:01 +0530

    Time: 34 ms (54.91%), Space: 13.9 MB (59.92%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,28 +1,35 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& prereq) {
-        int m = prereq.size();
-        vector<vector<int>>adj(n);
-        vector<int>indegree(n,0);
-        for(int i=0;i<m;i++){
-            adj[prereq[i][1]].push_back(prereq[i][0]);
-            indegree[prereq[i][0]]++;
+    bool canFinish(int n, vector<vector<int>>& prerequisites) {
+        vector<int>indeg(n,0);
+        vector<int>adj[n];
+        for(auto it:prerequisites){
+            adj[it[1]].push_back(it[0]);
+            indeg[it[0]]++;
         }
+        vector<bool>taken(n,false);
         queue<int>q;
         for(int i=0;i<n;i++){
-            if(indegree[i]==0) q.push(i);
+            if(indeg[i]==0){
+                taken[i] = true;
+                q.push(i);
+            }
         }
         while(!q.empty()){
-            int f = q.front(); q.pop();
-            for(auto it:adj[f]){
-                indegree[it]--;
-                if(indegree[it]==0){
-                    q.push(it);
+            int sz = q.size();
+            for(int i=0;i<sz;i++){
+                int f = q.front(); q.pop();
+                for(auto ad:adj[f]){
+                    indeg[ad]--;
+                    if(indeg[ad]==0){
+                        taken[ad] = true;
+                        q.push(ad);
+                    }
                 }
             }
         }
         for(int i=0;i<n;i++){
-            if(indegree[i]) return false;
+            if(!taken[i]) return false;
         }
         return true;
     }
