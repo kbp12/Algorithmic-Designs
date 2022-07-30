37859ca39ed999737f2e04dbdd248968cd4c9712
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-07-30 16:39:22 +0530

    Time: 142 ms (85.10%), Space: 43.4 MB (44.10%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -2,32 +2,33 @@ class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
         vector<pair<int,int>>adj[n];
-        for(int i=0;i<times.size();i++){
-            adj[times[i][0]-1].push_back({times[i][1]-1,times[i][2]});
+        vector<int>t(n,INT_MAX);
+        t[k-1] = 0;
+        for(auto time:times){
+            adj[time[0]-1].push_back({time[1]-1,time[2]});
         }
-        vector<bool>visited(n,false);
-        vector<int>time(n,1e9);
-        time[k-1] = 0;
+        vector<bool>visit(n,false);
         for(int i=0;i<n;i++){
             int idx = -1;
+            int val = INT_MAX;
             for(int j=0;j<n;j++){
-                if(visited[j]==false && (idx==-1 || time[j]<time[idx])){
+                if(visit[j]==false and t[j]<val){
                     idx = j;
+                    val = t[j];
                 }
             }
-            if(idx==-1 || time[idx]==1e9) return -1;
-            visited[idx] = true;
-            for(int p=0;p<adj[idx].size();p++){
-                int a = adj[idx][p].first;
-                int b = adj[idx][p].second;
-                time[a] = min(time[a],time[idx]+b);
+            if(idx==-1) return -1;
+            visit[idx] = true;
+            for(auto c:adj[idx]){
+                if(visit[c.first]==false and t[idx]+c.second<t[c.first]){
+                   t[c.first] = t[idx]+c.second;
+                }
             }
         }
-        int ans = -1;
+        int min_time = INT_MIN;
         for(int i=0;i<n;i++){
-            if(visited[i]==false) return -1;
-            ans = max(ans,time[i]);
+            min_time = max(min_time,t[i]);
         }
-        return ans;
+        return min_time;
     }
 };
