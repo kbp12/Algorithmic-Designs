aff9e75379ab426046a8e3a365e422b919eb850b
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-01 08:12:30 +0530

    Time: 203 ms (78.92%), Space: 57.2 MB (38.47%) - LeetHub

--- 310-minimum-height-trees/310-minimum-height-trees.cpp
+++ 310-minimum-height-trees/310-minimum-height-trees.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges){
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         if(n==1) return {0};
-        vector<vector<int>>adj(n);
-        vector<int>indegree(n,0);
-        for(auto e: edges){
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
-            indegree[e[0]]++;
-            indegree[e[1]]++;
+        vector<vector<int>>g(n);
+        vector<int>indeg(n,0);
+        for(auto edge:edges){
+            g[edge[0]].push_back(edge[1]);
+            g[edge[1]].push_back(edge[0]);
+            indeg[edge[0]]++;
+            indeg[edge[1]]++;
         }
-        vector<int>ans;
         queue<int>q;
+        vector<int>ans;
+        vector<int>temp;
         for(int i=0;i<n;i++){
-            if(indegree[i]==1){
+            if(indeg[i]==1){
                 q.push(i);
+                temp.push_back(i);
             }
         }
+        ans = temp;
         while(!q.empty()){
             int siz = q.size();
-            ans.clear();
+            ans = temp;
+            temp.clear();
             for(int i=0;i<siz;i++){
                 int f = q.front(); q.pop();
-                ans.push_back(f);
-                for(auto it:adj[f]){
-                    indegree[it]--;
-                    if(indegree[it]==1){
-                        q.push(it);
+                for(auto next:g[f]){
+                    indeg[next]--;
+                    if(indeg[next]==1){
+                        temp.push_back(next);
+                        q.push(next);
                     }
                 }
             }
