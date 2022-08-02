24c5cfb46d277ecf847563e4b44d73df03484cf5
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-02 15:34:53 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (82.49%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -1,8 +1,14 @@
 class Solution {
 public:
+    int gcd(int a,int b){
+        if(b>a) swap(a,b);
+        return b==0?a:gcd(b,a%b);
+    }
+    
     bool canMeasureWater(int j1, int j2, int t) {
         if(j1==t or j2==t or j1+j2==t) return true;
         if(t>j1+j2) return false;
-        return (t%(__gcd(j1,j2)))==0;
+        int gcdd = gcd(j1,j2);
+        return t%gcdd==0;
     }
 };
