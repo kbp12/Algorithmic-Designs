4349ab54ee70c7d543741bc2bbac7f2be8a95aa1
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-03 16:43:44 +0530

    Time: 270 ms (36.23%), Space: 91.7 MB (59.26%) - LeetHub

--- 565-array-nesting/565-array-nesting.cpp
+++ 565-array-nesting/565-array-nesting.cpp
@@ -1,41 +1,16 @@
 class Solution {
 public:
-    vector<int>dfsdepth;
-    vector<bool>visit;
-
-    void dfs(vector<int>& nums, int i){
-        if(nums[i]==0){
-            visit[i] = true;
-            dfsdepth[i] = 1;
-            return;
-        }
-        if(dfsdepth[nums[i]]!=-1){
-            dfsdepth[i] = dfsdepth[nums[i]]+1;
-            return;
-        }
-        if(visit[nums[i]] or nums[i]==i){
-            dfsdepth[i] = 1;
-            visit[i] = true;
-            return;
-        }
-        visit[i] = true;
-        dfs(nums,nums[i]);
-        dfsdepth[i] = dfsdepth[nums[i]]+1;
-        return;
-    }
-    
     int arrayNesting(vector<int>& nums) {
-        int n = nums.size();
-        dfsdepth = vector<int>(n,-1);
-        visit = vector<bool>(n,false);
-        for(int i=0;i<n;i++){
-            if(dfsdepth[i]==-1){
-                dfs(nums,i);
+        int n = nums.size(), ans = 0;
+        vector<bool>visit(n,false);
+        for(auto i:nums){
+            int dfsdepth = 0;
+            while(visit[i]==false){
+                dfsdepth++;
+                visit[i] = true;
+                i = nums[i];
             }
-        }
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            ans = max(ans,dfsdepth[i]);
+            ans = max(ans,dfsdepth);
         }
         return ans;
     }
