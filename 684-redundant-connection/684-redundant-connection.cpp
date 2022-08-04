ce93ce72ed80f84f18a8f87d5c5788f376f9f28a
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-04 11:43:10 +0530

    Time: 8 ms (82.35%), Space: 8.9 MB (54.90%) - LeetHub

---
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -0,0 +1,54 @@
+class DSU{
+	private:
+		vector<int> parent;
+		vector<int> rank;
+	public:
+        DSU(int n){
+            parent = vector<int>(n,-1);
+            rank = vector<int>(n,1);
+        }
+    
+		int find(int x){
+			if(parent[x]<0) return x;
+		    return parent[x]=find(parent[x]);
+		}
+    
+		void merge(int a,int b){
+			int parent_a = find(a);
+			int parent_b = find(b);
+			if(parent_a == parent_b){
+                return;
+            }
+			if(rank[parent_a] > rank[parent_b]){
+				parent[parent_b] = parent_a;
+			}else if(rank[parent_a] < rank[parent_b]){
+				parent[parent_a] = parent_b;
+			}
+			else{
+				parent[parent_b] = parent_a;
+				rank[parent_a]++;
+			}
+            return;
+		}
+};
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n = edges.size();
+        DSU dsu(n);
+        vector<int>ans;
+        for(int i=0;i<edges.size();i++){
+            int a = edges[i][0]-1, b = edges[i][1]-1;
+            int pa = dsu.find(a);
+            int pb = dsu.find(b);
+            if(pa==pb){
+                ans = edges[i];
+                break;
+            }else{
+                dsu.merge(a,b);
+            }
+        }
+        return ans;
+    }
+};
