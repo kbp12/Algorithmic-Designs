01cf9d7f85f6ef9ac5feace3324a35a3335146bf
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-12 18:52:33 +0530

    Time: 261 ms (73.76%), Space: 54 MB (27.90%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
+        int n = s.size();
+        vector<pair<int,int>>dp[26];
+        int i=0;
+        for(auto word:words){
+            dp[word[0]-'a'].push_back({i,0});
+            i++;
+        }
         int ans = 0;
-        int l1 = s.length();
-        map<string,int> m; 
-        for (auto word : words){
-            int l2 = word.length(); 
-            int j = 0;
-            if (m.find(word) == m.end()){ 
-               for (int i=0;i<l1 && j<l2;i++){   
-                    if (s[i]==word[j]){
-                        j++;
-                    }
+        for(int i=0;i<n;i++){
+            char ch = s[i];
+            auto it = dp[ch-'a'];
+            dp[ch-'a'].clear();
+            for(auto e:it){
+                int sz = words[e.first].size();
+                int idx = e.second;
+                if(idx==sz-1){
+                    ans++;
+                }else{
+                    idx++;
+                    int j = words[e.first][idx]-'a';
+                    dp[j].push_back({e.first,idx});
                 }
-                ans+=(j==l2);
-                m[word] = (j==l2);
             }
-            else{
-                ans+= m[word];
-            }
-            
-            
         }
         return ans;
     }
