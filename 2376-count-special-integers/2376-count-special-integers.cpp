fc5006449ffa6ec4e7c0c1d7a647e8c8e098fe17
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-15 07:30:21 +0530

    Time: 2 ms (100.00%), Space: 6 MB (60.00%) - LeetHub

--- 2376-count-special-integers/2376-count-special-integers.cpp
+++ 2376-count-special-integers/2376-count-special-integers.cpp
@@ -1,35 +1,45 @@
 class Solution {
 public:
-    int permutation(int m, int n) {
-        return n == 0 ? 1 : permutation(m, n - 1) * (m - n + 1);
-    }
-    int countSpecialNumbers(int N) {
-        vector<int> digit;
-        for (int i = N + 1; i > 0; i /= 10) {
-            digit.push_back(i % 10);
+    int countSpecialNumbers(int n) {
+        string s = "";
+        while(n>0){
+            int d = n%10;
+            s.push_back('0'+d);
+            n = n/10;
         }
-        reverse(digit.begin(), digit.end());
+        reverse(s.begin(),s.end());
+        int l = s.size();
+        int ans = 0;
+        for(int i=1;i<l;i++){
+            int temp = 9;
+            int k = 9;
+            for(int j=1;j<i;j++){
+                temp = temp*k; k--;
+            }
+            ans+=temp;
+        }
         
-        int result = 0;
-        int n = digit.size();
-        for (int i = 1; i < n; i++) {
-            result += 9 * permutation(9, i - 1);
-        }
-        unordered_set<int> seen;
-        for (int i = 0; i < n; i++) {
-            int dig = digit[i];
+        set<int>g;
+        for(int i=0;i<l;i++){
+            int it = s[i]-'0';
             int j = 0;
-            if(i==0) j++;
-            for ( ; j < dig; j++) {
-                if (seen.find(j) == seen.end()) {
-                    result += permutation(9-i, n - i - 1);
+            if(i==0) j=1;
+            for(; j<it;j++){
+                if(g.find(j)==g.end()){
+                    int rem = 10-(i);
+                    int lrem = l-(i);
+                    int temp = 1;
+                    for(int k=1;k<lrem;k++){
+                        rem--;
+                        temp = temp*rem;
+                    }
+                    ans+=temp;
                 }
             }
-            if (seen.find(digit[i]) != seen.end()) {
-                break;
-            }
-            seen.insert(digit[i]);
+            if(g.find(it)!=g.end()) break;
+            g.insert(it);
         }
-        return result;
+        if(g.size()==l) ans++;
+        return ans;
     }
 };
