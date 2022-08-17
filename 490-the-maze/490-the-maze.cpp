83cef2966713e95019490deeb0a6c11b4226bb3d
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-17 23:42:37 +0530

    Time: 62 ms (62.35%), Space: 19 MB (84.01%) - LeetHub

--- 490-the-maze/490-the-maze.cpp
+++ 490-the-maze/490-the-maze.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool bfs(int i, int j, int a, int b, vector<vector<int>>& maze, vector<vector<bool>>& visited, int n, int m){
-        if(i<0 or j<0 or i>=n or j>=m or maze[i][j]==1 or visited[i][j]) return false;
         if(i==a and j==b) return true;
         visited[i][j] = true;
         int ii = i;
         while(ii>0 and maze[ii-1][j]!=1){
             ii--;
         }
-        if(bfs(ii,j,a,b,maze,visited,n,m)) return true;
+        if(!visited[ii][j] and bfs(ii,j,a,b,maze,visited,n,m)) return true;
         ii = i;
         while(ii<n-1 and maze[ii+1][j]!=1){
             ii++;
         }
-        if(bfs(ii,j,a,b,maze,visited,n,m)) return true;
+        if(!visited[ii][j] and bfs(ii,j,a,b,maze,visited,n,m)) return true;
         int jj = j;
         while(jj>0 and maze[i][jj-1]!=1){
             jj--;
         }
-        if(bfs(i,jj,a,b,maze,visited,n,m)) return true;
+        if(!visited[i][jj] and bfs(i,jj,a,b,maze,visited,n,m)) return true;
         jj =j;
         while(jj<m-1 and maze[i][jj+1]!=1){
             jj++;
         }
-        if(bfs(i,jj,a,b,maze,visited,n,m)) return true;
+        if(!visited[i][jj] and bfs(i,jj,a,b,maze,visited,n,m)) return true;
         return false;
     }
     
