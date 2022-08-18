aa98720af513b3426b905e7b7b1d2615067dc0a0
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-18 07:18:52 +0530

    Time: 45 ms (85.02%), Space: 18.7 MB (94.74%) - LeetHub

--- 490-the-maze/490-the-maze.cpp
+++ 490-the-maze/490-the-maze.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool bfs(int i, int j, int a, int b, vector<vector<int>>& maze, vector<vector<bool>>& visited, int n, int m){
+    bool dfs(int i, int j, int a, int b, vector<vector<int>>& maze, vector<vector<bool>>& visited, int n, int m){
         if(i==a and j==b) return true;
         visited[i][j] = true;
         int ii = i;
         while(ii>0 and maze[ii-1][j]!=1){
             ii--;
         }
-        if(!visited[ii][j] and bfs(ii,j,a,b,maze,visited,n,m)) return true;
+        if(!visited[ii][j] and dfs(ii,j,a,b,maze,visited,n,m)) return true;
         ii = i;
         while(ii<n-1 and maze[ii+1][j]!=1){
             ii++;
         }
-        if(!visited[ii][j] and bfs(ii,j,a,b,maze,visited,n,m)) return true;
+        if(!visited[ii][j] and dfs(ii,j,a,b,maze,visited,n,m)) return true;
         int jj = j;
         while(jj>0 and maze[i][jj-1]!=1){
             jj--;
         }
-        if(!visited[i][jj] and bfs(i,jj,a,b,maze,visited,n,m)) return true;
+        if(!visited[i][jj] and dfs(i,jj,a,b,maze,visited,n,m)) return true;
         jj =j;
         while(jj<m-1 and maze[i][jj+1]!=1){
             jj++;
         }
-        if(!visited[i][jj] and bfs(i,jj,a,b,maze,visited,n,m)) return true;
+        if(!visited[i][jj] and dfs(i,jj,a,b,maze,visited,n,m)) return true;
         return false;
     }
     
@@ -30,6 +30,6 @@ public:
         int n = maze.size();
         int m = maze[0].size();
         vector<vector<bool>>visited(n,vector<bool>(m,false));
-        return bfs(start[0],start[1],destination[0],destination[1],maze,visited,n,m);
+        return dfs(start[0],start[1],destination[0],destination[1],maze,visited,n,m);
     }
 };
