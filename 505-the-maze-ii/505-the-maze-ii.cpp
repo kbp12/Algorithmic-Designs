e8351e6fa7ebebb1e096345e173c9f0bde895487
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-18 08:14:01 +0530

    Time: 1211 ms (7.48%), Space: 19.8 MB (80.75%) - LeetHub

--- 505-the-maze-ii/505-the-maze-ii.cpp
+++ 505-the-maze-ii/505-the-maze-ii.cpp
@@ -6,13 +6,13 @@ public:
         if(i==a and j==b) return;
         int l = j, r = j, u = i, d = i;
         while(l>0 and maze[i][l-1]!=1){ l--; }
-        if(l!=j) dfs(i,l,a,b,maze,dist,len+abs(l-j));
+        dfs(i,l,a,b,maze,dist,len+abs(l-j));
         while(u>0 and maze[u-1][j]!=1){ u--; }
-        if(u!=i) dfs(u,j,a,b,maze,dist,len+abs(i-u));
+        dfs(u,j,a,b,maze,dist,len+abs(i-u));
         while(r<maze[0].size()-1 and maze[i][r+1]!=1){ r++; }
-        if(j!=r) dfs(i,r,a,b,maze,dist,len+abs(j-r));
+        dfs(i,r,a,b,maze,dist,len+abs(j-r));
         while(d<maze.size()-1 and maze[d+1][j]!=1){ d++; }
-        if(d!=i) dfs(d,j,a,b,maze,dist,len+abs(i-d));
+        dfs(d,j,a,b,maze,dist,len+abs(i-d));
         return;
     }
     
