f7d9931b0fc89b5930bc781065c1aea01bdca8fc
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-19 00:28:45 +0530

    Time: 941 ms (49.55%), Space: 0B (100.00%) - LeetHub

--- 534-game-play-analysis-iii/534-game-play-analysis-iii.sql
+++ 534-game-play-analysis-iii/534-game-play-analysis-iii.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
 select a.player_id, a.event_date, sum(b.games_played) as games_played_so_far
-from activity a join activity b
+from Activity a join Activity b
 on a.player_id = b.player_id
 where a.player_id = b.player_id and a.event_date>=b.event_date
 group by a.player_id, a.event_date
 order by a.player_id, a.event_date;
-    
