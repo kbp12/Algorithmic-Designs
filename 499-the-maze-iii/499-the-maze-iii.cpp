a8d4de583726cf6c49790a6ca880d7eb31ba30f0
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-18 11:46:00 +0530

    Time: 1122 ms (5.36%), Space: 370.1 MB (6.55%) - LeetHub

---
+++ 499-the-maze-iii/499-the-maze-iii.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string ans = "";
+    void dfs(int i,int j,int a,int b, vector<vector<int>>& maze, vector<vector<int>>& dist, int len,string str){
+        if(dist[i][j]<len) return;
+        if(i==a and j==b and dist[i][j]==len){
+            ans = min(ans,str);
+            return;
+        }
+        dist[i][j] = len;
+        if(i==a and j==b){
+            ans = str;
+            return;
+        }  
+        int l = j, r = j, u = i, d = i;
+        while(l>0 and maze[i][l-1]!=1){
+            l--; 
+            if(l==b and i==a) 
+                break;
+        }
+        if(l!=j) dfs(i,l,a,b,maze,dist,len+abs(l-j),str+"l");
+        while(u>0 and maze[u-1][j]!=1){ 
+            u--;
+            if(u==a and j==b)
+                break;
+        }
+        if(u!=i) dfs(u,j,a,b,maze,dist,len+abs(i-u),str+"u");
+        while(r<maze[0].size()-1 and maze[i][r+1]!=1){ 
+            r++;
+            if(b==r and a==i){
+                break;
+            }
+        }
+        if(j!=r) dfs(i,r,a,b,maze,dist,len+abs(j-r),str+"r");
+        while(d<maze.size()-1 and maze[d+1][j]!=1){ 
+            d++;
+            if(d==a and j==b)
+                break;
+        }
+        if(d!=i) dfs(d,j,a,b,maze,dist,len+abs(i-d),str+"d");
+        return;
+    }
+    string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
+        int n = maze.size();
+        int m = maze[0].size();
+        vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
+        dfs(ball[0],ball[1],hole[0],hole[1],maze,dist,0,"");
+        //cout<<dist[hole[0]][hole[1]]<<endl;
+        return dist[hole[0]][hole[1]]==INT_MAX?"impossible":ans;
+    }
+};
