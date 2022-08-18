122174c20064c57f325a898acc63e940dc4e058b
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-18 11:49:19 +0530

    Time: 52 ms (25.60%), Space: 10.9 MB (100.00%) - LeetHub

--- 499-the-maze-iii/499-the-maze-iii.cpp
+++ 499-the-maze-iii/499-the-maze-iii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string ans = "";
-    void dfs(int i,int j,int a,int b, vector<vector<int>>& maze, vector<vector<int>>& dist, int len,string str){
+    void dfs(int i,int j,int a,int b, vector<vector<int>>& maze, vector<vector<int>>& dist, int len,string &str){
         if(dist[i][j]<len) return;
         if(i==a and j==b and dist[i][j]==len){
             ans = min(ans,str);
@@ -18,34 +18,50 @@ public:
             if(l==b and i==a) 
                 break;
         }
-        if(l!=j) dfs(i,l,a,b,maze,dist,len+abs(l-j),str+"l");
+        if(l!=j){
+            str+='l';
+            dfs(i,l,a,b,maze,dist,len+abs(l-j),str);
+            str.pop_back();
+        }
         while(u>0 and maze[u-1][j]!=1){ 
             u--;
             if(u==a and j==b)
                 break;
         }
-        if(u!=i) dfs(u,j,a,b,maze,dist,len+abs(i-u),str+"u");
+        if(u!=i){
+            str+='u';
+            dfs(u,j,a,b,maze,dist,len+abs(i-u),str);
+            str.pop_back();
+        }
         while(r<maze[0].size()-1 and maze[i][r+1]!=1){ 
             r++;
             if(b==r and a==i){
                 break;
             }
         }
-        if(j!=r) dfs(i,r,a,b,maze,dist,len+abs(j-r),str+"r");
+        if(j!=r){
+            str+='r';
+            dfs(i,r,a,b,maze,dist,len+abs(j-r),str);
+            str.pop_back();
+        }
         while(d<maze.size()-1 and maze[d+1][j]!=1){ 
             d++;
             if(d==a and j==b)
                 break;
         }
-        if(d!=i) dfs(d,j,a,b,maze,dist,len+abs(i-d),str+"d");
+        if(d!=i){
+            str+='d';
+            dfs(d,j,a,b,maze,dist,len+abs(i-d),str);
+            str.pop_back();
+        }
         return;
     }
     string findShortestWay(vector<vector<int>>& maze, vector<int>& ball, vector<int>& hole) {
         int n = maze.size();
         int m = maze[0].size();
         vector<vector<int>>dist(n,vector<int>(m,INT_MAX));
-        dfs(ball[0],ball[1],hole[0],hole[1],maze,dist,0,"");
-        //cout<<dist[hole[0]][hole[1]]<<endl;
+        string str = "";
+        dfs(ball[0],ball[1],hole[0],hole[1],maze,dist,0,str);
         return dist[hole[0]][hole[1]]==INT_MAX?"impossible":ans;
     }
 };
