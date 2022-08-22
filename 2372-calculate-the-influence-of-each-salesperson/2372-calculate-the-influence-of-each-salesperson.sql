cfc7b6ccb154430e47197451280f734097d75d1b
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-22 20:05:46 +0530

    Time: 1199 ms (46.15%), Space: 0B (100.00%) - LeetHub

---
+++ 2372-calculate-the-influence-of-each-salesperson/2372-calculate-the-influence-of-each-salesperson.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+with tab as(
+    select c.customer_id,c.salesperson_id, sum(d.price) as summ
+    from Customer c join Sales d
+    on c.customer_id = d.customer_id
+    group by c.salesperson_id
+)
+select a.salesperson_id, a.name, coalesce(b.summ,0) as total
+from Salesperson a left join tab b
+on a.salesperson_id = b.salesperson_id
+group by salesperson_id;
