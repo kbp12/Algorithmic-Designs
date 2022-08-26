3596a6aaa0e90429f74c1486d3a259692f6253f3
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-27 00:01:46 +0530

    Time: 23 ms (17.07%), Space: 9.6 MB (91.71%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        //space =  n*m
-        //space o(m);
-        //row1 = 4 5 6
-    //row1=>    row2 = 6 7 8
-    //row2=>    row3 = 7 8 9
-        vector<int>row1(n),row2(n);
-        row1[0] = grid[0][0];
-        for(int i=1;i<n;i++){
-            row1[i] = row1[i-1] + grid[0][i];
-        }
-        for(int i=1;i<m;i++){
-            row2[0] = row1[0] + grid[i][0];
-            for(int j=1;j<n;j++){
-                row2[j] = grid[i][j] + min(row2[j-1],row1[j]);
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i==0 && j ==0)
+                    continue;
+                else if(i == 0){
+                    grid[i][j] = grid[i][j] + grid[i][j-1];
+                }
+                else if(j == 0){
+                    grid[i][j] = grid[i][j] + grid[i-1][j];
+                }
+                else{
+                    grid[i][j] = grid[i][j]+min(grid[i-1][j],grid[i][j-1]);
+                }
             }
-            swap(row1,row2);
         }
-        return row1[n-1];
+        return grid[n-1][m-1];
     }
 };
