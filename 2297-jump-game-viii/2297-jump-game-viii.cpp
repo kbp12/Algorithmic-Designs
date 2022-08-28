173663f0f320f31908591d844a38176442e683eb
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-08-28 18:31:12 +0530

    Time: 353 ms (44.72%), Space: 100.9 MB (47.97%) - LeetHub

---
+++ 2297-jump-game-viii/2297-jump-game-viii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long minCost(vector<int>& nums, vector<int>& costs) {
+        int n = nums.size();
+        vector<long long>dp(n,LONG_MAX);
+        dp[0] = 0;
+        deque<long long>one, two;
+        for(int i=0;i<n;i++){
+            while(one.size() and nums[i]>=nums[one.back()]){
+                dp[i] = min(dp[i],dp[one.back()]+costs[i]);
+                one.pop_back();
+            }
+            while(two.size() and nums[i]<nums[two.back()]){
+                dp[i] = min(dp[i],dp[two.back()]+costs[i]);
+                two.pop_back();
+            }
+            one.push_back(i); 
+            two.push_back(i);
+        }
+        return dp[n-1];
+    }
+};
+
