4fdb8a9913cb46b685aedc50ce0b27e89d98a71b
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-02 07:39:25 +0530

    Time: 3 ms (46.50%), Space: 6.1 MB (56.74%) - LeetHub

--- 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/678-valid-parenthesis-string.cpp
@@ -3,6 +3,7 @@ public:
     bool checkValidString(string s) {
         int n = s.length();
         int l = 0, r = 0, stars = 0;
+        //cheecking if i am maximizing left count
         for(int i=0;i<n;i++){
             if(s[i]=='*')   stars++;
             else if(s[i]==')')  r++;
@@ -10,6 +11,7 @@ public:
             if(r-l>stars) return false;
         }
         l=0,r=0,stars=0;
+        //maximizing right count
         for(int i=n-1;i>=0;i--){
             if(s[i]=='*') stars++;
             else if(s[i]==')')  r++;
