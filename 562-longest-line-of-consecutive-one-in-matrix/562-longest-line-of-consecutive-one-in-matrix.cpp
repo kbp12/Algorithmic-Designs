f952fd5c481d93dd693ae87a5333309c962f62f4
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-05 10:52:10 +0530

    Time: 194 ms (24.07%), Space: 31.2 MB (46.41%) - LeetHub

---
+++ 562-longest-line-of-consecutive-one-in-matrix/562-longest-line-of-consecutive-one-in-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int>dx = {1,0,1,-1};
+    vector<int>dy = {0,1,1,1};
+    int search(vector<vector<int>>grid, int k){
+        int n = grid.size(), m = grid[0].size(), ans = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1){
+                    int a = i-dy[k], b = j-dx[k];
+                    if(a>=0 and b>=0 and a<n and b<m){
+                        grid[i][j]+=grid[a][b];
+                    }
+                    ans = max(ans,grid[i][j]);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    int longestLine(vector<vector<int>>& mat) {
+        int ans = 0;
+        for(int k=0;k<4;k++){
+            ans = max(ans,search(mat,k));
+        }
+        return ans;
+    }
+};
