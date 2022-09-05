fd53bb5eecc8513b534fee75312c16c4dbbb954e
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-05 08:37:29 +0530

    Time: 253 ms (28.67%), Space: 76.9 MB (21.68%) - LeetHub

--- 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
+++ 1746-maximum-subarray-sum-after-one-operation/1746-maximum-subarray-sum-after-one-operation.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maxSumAfterOperation(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(2,0));
+        vector<vector<int>>dp(2,vector<int>(n,0));
         dp[0][0] = nums[0];
-        dp[0][1] = nums[0]*nums[0];
-        int ans = dp[0][1];
+        dp[1][0] = nums[0]*nums[0];
+        int ans = dp[1][0];
         for(int i=1;i<n;i++){
-            dp[i][0] = max(nums[i],dp[i-1][0]+nums[i]);
-            dp[i][1] = max({nums[i]*nums[i],dp[i-1][0]+nums[i]*nums[i],dp[i-1][1]+nums[i]});
-            ans = max({ans,dp[i][0],dp[i][1]});
+            dp[0][i] = max(nums[i],dp[0][i-1]+nums[i]);
+            dp[1][i] = max({nums[i]*nums[i],dp[0][i-1]+nums[i]*nums[i],dp[1][i-1]+nums[i]});
+            ans = max({ans,dp[0][i],dp[1][i]});
         }
         return ans;
     }
