72f1f1462585e338d7c9df4010e4b4033f571f84
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-08 08:34:10 +0530

    Time: 1932 ms (11.96%), Space: 0B (100.00%) - LeetHub

--- 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
+++ 1083-sales-analysis-ii/1083-sales-analysis-ii.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
-SELECT b.buyer_id
-FROM Product AS a
-JOIN Sales AS b
-ON a.product_id = b.product_id 
-GROUP BY b.buyer_id 
-HAVING SUM(a.product_name = 'S8') > 0 and SUM(a.product_name = 'iPhone') = 0;
+SELECT s.buyer_id
+FROM Sales AS s INNER JOIN Product AS p
+ON s.product_id = p.product_id
+GROUP BY s.buyer_id
+HAVING SUM(CASE WHEN p.product_name = 'S8' THEN 1 ELSE 0 END) > 0
+AND SUM(CASE WHEN p.product_name = 'iPhone' THEN 1 ELSE 0 END) = 0;
