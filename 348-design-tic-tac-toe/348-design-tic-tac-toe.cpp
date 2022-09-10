4ec58b2d91263e68b068128f94f1abd5b53d8375
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-10 18:26:39 +0530

    Time: 64 ms (15.37%), Space: 19.4 MB (47.32%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
@@ -13,13 +13,13 @@ public:
         if(p==1){
             row[r]++;
             col[c]++;
-            if(r==c){ dia++;}
+            if(r==c) dia++;
             if(r+c==n-1) antidia++;
             if(row[r]==n or col[c]==n or dia==n or antidia==n){ return p;}
         }else{
             row[r]--;
             col[c]--;
-            if(r==c) {dia--;}
+            if(r==c) dia--;
             if(r+c==n-1) antidia--;
             if(row[r]==-n or col[c]==-n or dia==-n or antidia==-n){ return p;}
         }
