4a2af12f0845a5bb106d47517d004e6ea7ff2c64
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-10 18:25:35 +0530

    Time: 27 ms (92.01%), Space: 19.3 MB (47.32%) - LeetHub

--- 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
+++ 348-design-tic-tac-toe/348-design-tic-tac-toe.cpp
@@ -1,52 +1,29 @@
 class TicTacToe {
 public:
-    int n;
-    vector<vector<int>>v;
-    TicTacToe(int N) {
-        n = N;
-        v = vector<vector<int>>(n,vector<int>(n,0));
-        return;
+    vector<int>row, col;
+    int dia, antidia, n;
+    TicTacToe(int sz) {
+        n = sz;
+        dia = antidia = 0;
+        row.resize(n,0);
+        col.resize(n,0);
     }
     
-    vector<int>dx = {-1,1,0,0,-1,1,-1,1};
-    vector<int>dy = {0,0,-1,1,-1,1,1,-1};
-    
-    int move(int r, int c, int p){
-        v[r][c] = p;
-        vector<int>idx = {0,2};
-        if(r==c and r+c==n-1){
-            idx.push_back(4);
-            idx.push_back(6);
-        }else if(r==c){
-            idx.push_back(4);
-        }else if(r+c==n-1){
-            idx.push_back(6);
+    int move(int r, int c, int p) {
+        if(p==1){
+            row[r]++;
+            col[c]++;
+            if(r==c){ dia++;}
+            if(r+c==n-1) antidia++;
+            if(row[r]==n or col[c]==n or dia==n or antidia==n){ return p;}
+        }else{
+            row[r]--;
+            col[c]--;
+            if(r==c) {dia--;}
+            if(r+c==n-1) antidia--;
+            if(row[r]==-n or col[c]==-n or dia==-n or antidia==-n){ return p;}
         }
-        for(int t=0;t<idx.size();t++){
-            int k = idx[t];
-            int x = k, y = k+1;
-            int i = r, j = c;
-            bool yes = true;
-            while(i>=0 and j>=0 and i<n and j<n){
-                if(v[i][j]!=p){
-                    yes = false;
-                    break;
-                }
-                i+=dx[x];
-                j+=dy[x];
-            }
-            i = r, j = c;
-            while(i>=0 and j>=0 and i<n and j<n){
-                if(v[i][j]!=p){
-                    yes = false;
-                    break;
-                }
-                i+=dx[y];
-                j+=dy[y];
-            }
-            if(yes) return p;
-        }
-         return 0;
+        return 0;
     }
 };
 
