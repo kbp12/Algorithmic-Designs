132cabe694e758c0585418fd1eca93208ebdd4c1
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-11 19:50:12 +0530

    Time: 12 ms (72.99%), Space: 8.5 MB (37.96%) - LeetHub

--- 1066-campus-bikes-ii/1066-campus-bikes-ii.cpp
+++ 1066-campus-bikes-ii/1066-campus-bikes-ii.cpp
@@ -1,37 +1,25 @@
 class Solution {
 public:
-    int ans;
-    void dfs(int workers[][2], int bikes[][2], bool assigned[],int idx, int sum, int n, int m){
-        if(idx == n){
-            ans = min(ans,sum);
-            return;
-        }
-        if(sum>=ans) return;
-        for(int i=0;i<m;i++){
-            if(assigned[i]==false){
-                int temp = abs(bikes[i][0]-workers[idx][0])+abs(bikes[i][1]-workers[idx][1]);
-                sum+=temp;
-                
-                assigned[i] = true;
-                dfs(workers,bikes,assigned,idx+1,sum,n,m);
-                assigned[i] = false;
-                sum-=temp;
+    vector<int>dpmask;
+    
+    int dfs(vector<vector<int>>& workers, vector<vector<int>>& bikes, int widx, int mask){
+        if(widx==workers.size()) return 0;
+        if(dpmask[mask]!=-1) return dpmask[mask];
+        int minsum = INT_MAX;
+        for(int b=0;b<bikes.size();b++){
+            if((mask & (1<<b)) ==0){
+                int add = abs(workers[widx][0]-bikes[b][0])+abs(workers[widx][1]-bikes[b][1]);
+                int this_sum = dfs(workers,bikes,widx+1,(mask + (1<<b)))+add;
+                minsum = min(minsum,this_sum);
             }
         }
-        return;
+        return dpmask[mask] = minsum;
     }
     
-    int assignBikes(vector<vector<int>>& w, vector<vector<int>>& b) {
-        int n = w.size();
-        int workers[n][2];
-        for(int i=0;i<n;i++) workers[i][0] = w[i][0], workers[i][1] = w[i][1];
-        int m = b.size();
-        int bikes[m][2];
-        for(int i=0;i<m;i++) bikes[i][0] = b[i][0], bikes[i][1] = b[i][1];
-        ans = INT_MAX;
-        bool assigned[m];
-        for(int i=0;i<m;i++) assigned[i] = false;
-        dfs(workers,bikes,assigned,0,0,n,m);
-        return ans;
+    int assignBikes(vector<vector<int>>& workers, vector<vector<int>>& bikes) {
+        int m = bikes.size();
+        m = pow(2,m);
+        dpmask = vector<int>(m,-1);
+        return dfs(workers,bikes,0,0);
     }
 };
