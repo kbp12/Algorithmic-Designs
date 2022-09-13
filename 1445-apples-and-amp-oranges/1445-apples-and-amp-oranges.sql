05d4c391204a72803e5221b6571bc9ac6a5fca4a
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-13 19:46:01 +0530

    Time: 435 ms (41.72%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select a.sale_date , sum(a.sold_num)-sum(b.sold_num) as diff
+select a.sale_date , a.sold_num-b.sold_num as diff
 from Sales a join Sales b
 where a.sale_date = b.sale_date and a.fruit = 'apples' and b.fruit = 'oranges'
 group by a.sale_date;
