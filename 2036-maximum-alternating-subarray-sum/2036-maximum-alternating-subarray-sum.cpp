00e4b9bb8d4cef2d0fce8913bbf3bcff791c83e7
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-14 20:43:12 +0530

    Time: 590 ms (6.25%), Space: 111.3 MB (6.25%) - LeetHub

--- 2036-maximum-alternating-subarray-sum/2036-maximum-alternating-subarray-sum.cpp
+++ 2036-maximum-alternating-subarray-sum/2036-maximum-alternating-subarray-sum.cpp
@@ -2,18 +2,17 @@ class Solution {
 public:
     long long maximumAlternatingSubarraySum(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<long long>>dp(2,vector<long long>(n,INT_MIN));
+        vector<vector<long long>>dp(n,vector<long long>(2,INT_MIN));
         dp[0][0] = nums[0];
         long long ans = dp[0][0];
         for(int i=1;i<n;i++){
-            dp[0][i] = max(dp[1][i-1]+nums[i],(long long)nums[i]);
+            dp[i][0] = max(dp[i-1][1]+nums[i],(long long)nums[i]);
             if(i<2){
-                dp[1][i] = dp[0][i-1]-nums[i];
+                dp[i][1] = dp[i-1][0]-nums[i];
             }else{
-                dp[1][i] = max(dp[0][i-1]-nums[i],(long long)nums[i-1]-nums[i]);
+                dp[i][1] = max(dp[i-1][0]-nums[i],(long long)nums[i-1]-nums[i]);
             }
-            //dp[1][i] = max(dp[0][i-1]-nums[i],(long long)-nums[i]);
-            ans = max({ans,dp[0][i],dp[1][i]});
+            ans = max({ans,dp[i][0],dp[i][1]});
         }
         return ans;
     }
