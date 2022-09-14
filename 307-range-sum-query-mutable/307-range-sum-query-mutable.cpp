13df5be0584c116f8d0935fa9436dd544037c877
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-14 08:28:58 +0530

    Time: 1143 ms (5.01%), Space: 175.5 MB (8.56%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,52 +1,50 @@
 class NumArray {
 public:
-    vector<int>nums;
-    vector<int>sums;
+    vector<int>arr;
     int n;
-    int buildst(int l, int r, int i){
+    vector<int>seg;
+    int buildseg(int l, int r, int idx){
         if(l>r) return 0;
         if(l==r){
-            sums[i] = nums[l];
-            return sums[i];
+            seg[idx] = arr[l];
+            return seg[idx];
         }
         int m = l+(r-l)/2;
-        sums[i] = buildst(l,m,2*i+1) + buildst(m+1,r,2*i+2);
-        return sums[i];
+        seg[idx] = buildseg(l,m,2*idx+1)+buildseg(m+1,r,2*idx+2);
+        return seg[idx];
     }
     
-    void updatest(int l, int r, int diff, int i, int idx){
-        if(idx<l || idx>r) return;
-        sums[i]+=diff;
+    NumArray(vector<int>& nums) {
+        arr = nums;
+        n = arr.size();
+        seg.resize(4*n);
+        buildseg(0,n-1,0);
+        return;
+    }
+    
+    void updateseg(int l, int r, int idx, int index, int diff){
+        if(index<l or index>r) return;
+        seg[idx]+=diff;
         if(l==r) return;
         int m = l+(r-l)/2;
-        updatest(l,m,diff,2*i+1,idx);
-        updatest(m+1,r,diff,2*i+2,idx);
+        updateseg(l,m,2*idx+1,index,diff);
+        updateseg(m+1,r,2*idx+2,index,diff);
         return;
     }
-    
-    int sumst(int start, int end, int l, int r, int i){
-        if(l>end || r<start) return 0;
-        if(l<=start && end<=r) return sums[i];
-        int mid = start+(end-start)/2;
-        return sumst(start,mid,l,r,2*i+1)+sumst(mid+1,end,l,r,2*i+2);
+    void update(int index, int val) {
+        int diff = val-arr[index];
+        arr[index] = val;
+        updateseg(0,n-1,0,index,diff);
     }
     
-    NumArray(vector<int>& nums2){
-        n = nums2.size();
-        nums = nums2;
-        sums.resize(4*n);
-        buildst(0,n-1,0);
+    int sumseg(int left, int right, int l, int r, int idx){
+        if(right<l or left>r) return 0;
+        if(left<=l and r<=right) return seg[idx];
+        int m = l+(r-l)/2;
+        return sumseg(left,right,l,m,2*idx+1)+sumseg(left,right,m+1,r,2*idx+2);
     }
-    
-    void update(int index, int val){
-        int diff = val-nums[index];
-        nums[index] = val;
-        updatest(0,n-1,diff,0,index);
-        return;
-    }
-    
-    int sumRange(int l, int r){
-        return sumst(0,n-1,l,r,0);
+    int sumRange(int left, int right) {
+        return sumseg(left,right,0,n-1,0);  
     }
 };
 
