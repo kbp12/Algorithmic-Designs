d31da8c12ad2d12fee7e8aaed3560aad3144982b
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-14 10:33:11 +0530

    Time: 594 ms (19.00%), Space: 245 MB (11.15%) - LeetHub

--- 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.cpp
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.cpp
@@ -1,46 +1,36 @@
 class Solution {
-public: 
-    const static int N = 100001;
-    array<int, 2*N> seg{};
-    
-    void update(int pos, int val){ // update max
-        pos += N;
-        seg[pos] = val;
- 
-        while (pos > 1) {
-            pos >>= 1;
-            seg[pos] = max(seg[2*pos], seg[2*pos+1]);
-        }
-    }
- 
-    int query(int lo, int hi){ // query max [lo, hi)
-        lo += N;
-        hi += N;
-        int res = 0;
- 
-        while (lo < hi) {
-            if (lo & 1) {
-                res = max(res, seg[lo++]);
-            }
-            if (hi & 1) {
-                res = max(res, seg[--hi]);
-            }
-            lo >>= 1;
-            hi >>= 1;
-        }
-        return res;
+public:
+    vector<int>seg;
+    vector<int>arr;
+    int n;
+    int maxseg(int l,int r, int wl, int wr, int idx){
+        if(wr<l or wl>r) return 0;
+        if(wl<=l and r<=wr) return seg[idx];
+        int m = l+(r-l)/2;
+        return max(maxseg(l,m,wl,wr,2*idx+1),maxseg(m+1,r,wl,wr,2*idx+2));
     }
     
-    int lengthOfLIS(vector<int>& A, int k) {
+    void update(int l, int r, int idx, int arr_idx, int val){
+        if(arr_idx<l or arr_idx>r) return;
+        seg[idx]= max(seg[idx],val);
+        if(l==r) return;
+        int m = l+(r-l)/2;
+        update(l,m,2*idx+1,arr_idx,val);
+        update(m+1,r,2*idx+2,arr_idx,val);
+        return;
+    }
+    
+    int lengthOfLIS(vector<int>& nums, int k) {
+        n = 100001;
+        arr.resize(n,0);
+        seg.resize(4*n,0);
         int ans = 0;
-        for (int i = 0; i < size(A); ++i){
-            int l = max(0, A[i]-k);
-            int r = A[i];
-            int res = query(l, r) + 1; // best res for the current element
-            ans = max(res, ans);
-            update(A[i], res); // and update it here
+        for(auto num:nums){
+            int res = maxseg(1,100000,max(1,num-k),num-1,0)+1;
+            ans = max(ans,res);
+            arr[num] = res;
+            update(1,100000,0,num,res);
         }
-        
         return ans;
     }
 };
