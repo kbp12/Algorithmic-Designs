5610c30080f21c5dbf3bd48c6004bbded4bf42ff
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-14 16:55:36 +0530

    Time: 5 ms (33.33%), Space: 5.8 MB (100.00%) - LeetHub

--- 2005-subtree-removal-game-with-fibonacci-tree/2005-subtree-removal-game-with-fibonacci-tree.cpp
+++ 2005-subtree-removal-game-with-fibonacci-tree/2005-subtree-removal-game-with-fibonacci-tree.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool findGameWinner(int n) {
-        if((n-1)%6==0) return false;
-        return true;
+        if((n-1)%6) return true;
+        return false;
     }
 };
