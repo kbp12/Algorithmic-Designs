6a93660c1f92efcb126ae29d5bb8a1ebab365661
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-14 11:05:02 +0530

    Time: 288 ms (5.37%), Space: 12.5 MB (65.85%) - LeetHub

---
+++ 308-range-sum-query-2d-mutable/308-range-sum-query-2d-mutable.cpp
@@ -0,0 +1,28 @@
+class NumMatrix {
+public:
+    vector<vector<int>>grid;
+    NumMatrix(vector<vector<int>>& matrix) {
+        grid = matrix;
+    }
+    
+    void update(int row, int col, int val) {
+        grid[row][col] = val;
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for(int r=row1;r<=row2;r++){
+            for(int c=col1;c<=col2;c++){
+                sum+=grid[r][c];
+            }
+        }
+        return sum;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * obj->update(row,col,val);
+ * int param_2 = obj->sumRegion(row1,col1,row2,col2);
+ */
