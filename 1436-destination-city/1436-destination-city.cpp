b28f8d7902d29290fdf33c7d395bbc8d7559c4c6
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-09-22 19:51:27 +0530

    Time: 42 ms (5.09%), Space: 11.8 MB (5.69%) - LeetHub

--- 1436-destination-city/1436-destination-city.cpp
+++ 1436-destination-city/1436-destination-city.cpp
@@ -1,20 +1,9 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string,string>umap;
         set<string>s;
-        for(auto path:paths){
-            umap[path[0]] = path[1];
-            s.insert(path[1]);
-        }
-        string start = "";
-        for(auto it:umap){
-            if(s.find(it.first)==s.end()){
-                start = it.first;
-                break;
-            }
-        }
-        while(umap.find(start)!=umap.end()) start = umap[start];
-        return start;
+        for(auto path:paths) s.insert(path[1]);
+        for(auto path:paths) s.erase(path[0]);
+        return *s.begin();
     }
 };
