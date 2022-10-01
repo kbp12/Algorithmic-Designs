9e41c5ad9e7b6994b9b442998477ff85a31d66b5
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-01 12:22:07 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (49.09%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        vector<int> dp(s.size()+1);
-        dp[0]=1;
-        if(s[0]=='0')dp[1]=0;
-        else dp[1]=1;
-        for(int i=2;i<=s.size();i++){
-            int way1,way2;
-            if(s[i-1]=='0')way1=0;
-            else way1=dp[i-1];
-            if(stoi(s.substr(i-2,2))<=26&&stoi(s.substr(i-2,2))>0&&s[i-2]!='0')way2=dp[i-2];
-            else way2=0;
-            dp[i]=way1+way2;
+        int n = s.size();
+        vector<int>dp(n+1,0);
+        dp[0] = 1;
+        if(s[0]!='0') dp[1] = 1;
+        for(int i=2;i<=n;i++){
+            int way1 = 0, way2 = 0;
+            if(s[i-1]!='0') way1 = dp[i-1];
+            int a = stoi(s.substr(i-2,2));
+            if(a<27 and s[i-2]!='0') way2 = dp[i-2];
+            dp[i] = way1+way2;
         }
-        return dp[s.size()];
+        return dp[n];
     }
 };
-
