e2fececce7f9bba641288dd3574c92c2c0fe834c
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-02 12:01:44 +0530

    Time: 299 ms (100.00%), Space: 151.3 MB (62.50%) - LeetHub

--- 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -7,8 +7,8 @@ public:
         if(dp[siz]) return dp[siz];
         auto z_arr = z_f(s);
         int ans = 1;
-        for(int i=1;i<siz;i++){
-            if(i%2==1 and z_arr[i] == (i+1)/2){
+        for(int i=1;i<siz;i+=2){
+            if(z_arr[i] == (i+1)/2){
                 ans = max(ans,1+find(s.substr(z_arr[i])));
             }
         }
