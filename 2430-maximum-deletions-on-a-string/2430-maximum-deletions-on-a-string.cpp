05bae4035f57561f2471c396fc0962b56619bc9f
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-02 11:52:16 +0530

    Time: 458 ms (87.50%), Space: 151.3 MB (62.50%) - LeetHub

--- 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
+++ 2430-maximum-deletions-on-a-string/2430-maximum-deletions-on-a-string.cpp
@@ -1,29 +1,35 @@
 class Solution {
 public:
-    int n;
     int dp[4005] = {};
-    int f(string_view s, int l){
-        if (l== n-1){
-            return 1;
-        }
-        if(dp[l]) return dp[l];
-        int cnt = 1;
-        string str = "";
+    int find(string s){
+        int siz = s.size();
+        if(siz==0) return 0;
+        if(dp[siz]) return dp[siz];
+        auto z_arr = z_f(s);
         int ans = 1;
-        for (int i = l; i < n; i++) {
-            str.push_back(s[i]);
-            string_view s2 =s.substr(i + 1, str.size());
-            if (s2 == str) {
-                cnt = 1 + f(s,i+1);
-                ans = max(ans,cnt);
+        for(int i=1;i<siz;i++){
+            if(i%2==1 and z_arr[i] == (i+1)/2){
+                ans = max(ans,1+find(s.substr(z_arr[i])));
             }
         }
-        return dp[l] = ans;
+        return dp[siz] = ans;
     }
+    
+    vector<int> z_f(string s){
+        int n = s.size();
+        vector<int> dp(n,0);
+        for (int i=1;i<n;i++) {
+            int j = dp[i-1];
+            while(j>0 and s[i]!=s[j])
+                j = dp[j-1];
+            if (s[i]==s[j])
+                j++;
+            dp[i] = j;
+        }
+        return dp;
+    }
+    
     int deleteString(string s) {
-        n = s.size();
-        string_view s1{s};
-        return f(s1,0);
+        return find(s);
     }
-
 };
