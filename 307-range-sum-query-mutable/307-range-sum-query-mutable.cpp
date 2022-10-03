ce4001088411d96aef9c98a024caa3aa7fbe3bec
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-03 21:56:50 +0530

    Time: 1166 ms (17.92%), Space: 175.5 MB (22.14%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.cpp
@@ -1,50 +1,52 @@
 class NumArray {
 public:
-    vector<int>arr;
-    int n;
+    vector<int> nums;
+    int n; 
     vector<int>seg;
     int buildseg(int l, int r, int idx){
         if(l>r) return 0;
         if(l==r){
-            seg[idx] = arr[l];
+            seg[idx] = nums[l];
             return seg[idx];
         }
-        int m = l+(r-l)/2;
-        seg[idx] = buildseg(l,m,2*idx+1)+buildseg(m+1,r,2*idx+2);
+        int mid = l+(r-l)/2;
+        seg[idx] = buildseg(l,mid,2*idx+1) + buildseg(mid+1,r,2*idx+2);
         return seg[idx];
     }
     
-    NumArray(vector<int>& nums) {
-        arr = nums;
-        n = arr.size();
-        seg = vector<int>(4*n,0);
+    NumArray(vector<int>& nums2) {
+        nums = nums2;
+        n = nums.size();
+        seg.resize(4*n);
         buildseg(0,n-1,0);
-        return;
     }
     
-    void updateseg(int l, int r, int idx, int index, int diff){
-        if(index<l or index>r) return;
+    void updateseg(int l, int r, int i, int idx, int diff){
+        if(i<l or i>r) return;
         seg[idx]+=diff;
         if(l==r) return;
-        int m = l+(r-l)/2;
-        updateseg(l,m,2*idx+1,index,diff);
-        updateseg(m+1,r,2*idx+2,index,diff);
+        int mid = l+(r-l)/2;
+        updateseg(l,mid,i,2*idx+1,diff);
+        updateseg(mid+1,r,i,2*idx+2,diff);
         return;
     }
+    
     void update(int index, int val) {
-        int diff = val-arr[index];
-        arr[index] = val;
-        updateseg(0,n-1,0,index,diff);
+        int diff = val - nums[index];
+        nums[index] = val;
+        updateseg(0,n-1,index,0,diff);
+        return;
     }
     
-    int sumseg(int left, int right, int l, int r, int idx){
-        if(right<l or left>r) return 0;
-        if(left<=l and r<=right) return seg[idx];
-        int m = l+(r-l)/2;
-        return sumseg(left,right,l,m,2*idx+1)+sumseg(left,right,m+1,r,2*idx+2);
+    int sumseg(int in, int out, int l, int r, int idx){
+        if(in>r or out<l) return 0;
+        if(in<=l and r<=out) return seg[idx];
+        int mid = l + (r-l)/2;
+        return sumseg(in, out, l,mid, 2*idx+1) + sumseg(in, out , mid+1, r, 2*idx+2);
     }
+    
     int sumRange(int left, int right) {
-        return sumseg(left,right,0,n-1,0);  
+        return sumseg(left,right,0,n-1, 0);
     }
 };
 
