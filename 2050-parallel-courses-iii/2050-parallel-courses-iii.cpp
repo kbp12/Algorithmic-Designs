83b9be19d4a851fbf625b08101b14c85103f8044
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-20 00:37:01 +0530

    Time: 1089 ms (16.63%), Space: 143.4 MB (43.45%) - LeetHub

---
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int minimumTime(int n, vector<vector<int>>& relations, vector<int>& time) {
+        vector<int>adj[n+1];
+        vector<int>ind(n+1,0);
+        for(auto rel:relations){
+            adj[rel[1]].push_back(rel[0]);
+            ind[rel[0]]++;
+        }
+        vector<int>finish(n+1,-1);
+        queue<int>q;
+        for(int i=1;i<=n;i++){
+            if(ind[i]==0){
+                q.push(i);
+                finish[i] = time[i-1];
+            }
+        }
+        while(q.size()){
+            int f = q.front(); q.pop();
+            for(auto it:adj[f]){
+                ind[it]--;
+                if(finish[it]==-1) finish[it] = finish[f] + time[it-1];
+                else finish[it] = max(finish[it] , finish[f]+time[it-1]);
+                if(ind[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        return *max_element(finish.begin(),finish.end());
+    }
+};
