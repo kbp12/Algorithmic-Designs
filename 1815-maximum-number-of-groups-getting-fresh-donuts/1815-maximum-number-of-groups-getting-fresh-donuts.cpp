a4e738aef614a497b9070b48af1bcf217e93f41b
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-25 23:18:09 +0530

    Time: 9 ms (82.38%), Space: 8.7 MB (66.19%) - LeetHub

--- 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.cpp
+++ 1815-maximum-number-of-groups-getting-fresh-donuts/1815-maximum-number-of-groups-getting-fresh-donuts.cpp
@@ -1,42 +1,31 @@
 class Solution {
 public:
-    int maxHappyGroups(int n, vector<int>& v) {
-        vector<int> cnt(n, 0);
-        for(auto x:v) cnt[x % n]++;
-        State state;
-        for(int i=1;i<n;i++) state.add(i, cnt[i]);
-        return dp(0, n, state) + cnt[0];
+    map<vector<int>, int> dp;
+int dfs(vector<int>& cnt, int left) {
+    auto it = dp.find(cnt);
+    if (it != end(dp))
+        return it->second;
+    int res = 0, bz = cnt.size();
+    for (auto j = 1; j < bz; ++j) {
+        if (--cnt[j] >= 0)
+            res = max(res, (left == 0) + dfs(cnt, (bz + left - j) % bz));
+        ++cnt[j];
     }
-    
-    struct State {
-        bool empty(int i) const{
-            auto x = mask >> (i*5);
-            return (x & 0b11111) == 0;
+    return dp[cnt] = res;
+}
+int maxHappyGroups(int bz, vector<int>& groups) {
+    vector<int> cnt(bz);
+    int res = 0;
+    for (auto group : groups) {
+        if (group % bz == 0)
+            ++res;
+        else if (cnt[bz - group % bz]) {
+            --cnt[bz - group % bz];
+            ++res;
         }
-        
-        void add(int i, long long val){
-            mask += val << (i*5);
-        }
-        
-        State minus1(int i) const{
-            auto res = *this;
-            res.mask -= 1LL<<(i*5);
-            return res;
-        }
-        
-        long long mask = 0;
-    };
-    
-    int dp(int numDonut, int n, State state){
-        if(state.mask == 0) return 0;
-        if(mdp.count(state.mask)) return mdp[state.mask];
-        auto& res = mdp[state.mask];
-        for(int i=1;i<n;i++)
-            if(!state.empty(i)) res = max(res, dp((numDonut + i)%n, n, state.minus1(i)));
- 
-        res += (numDonut == 0) ? 1 : 0;
-        return res; 
+        else 
+            ++cnt[group % bz];
     }
-
-    unordered_map<long long, int> mdp; //numDonut, state    
+    return dfs(cnt, 0) + res;
+}
 };
