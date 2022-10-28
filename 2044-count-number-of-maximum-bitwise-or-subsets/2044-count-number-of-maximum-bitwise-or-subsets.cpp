d191684aa8600963c0ffc4df004d489102950e7e
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-28 21:57:03 +0530

    Time: 189 ms (23.79%), Space: 8.5 MB (50.51%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
-    int maxi, ans;
-    void backtrack(vector<bool>& v , vector<int>& nums , int idx){
-        if(idx == nums.size()){
-            int orr = 0;
-            for(int i=0;i<nums.size();i++){
-                if(v[i]) orr |= nums[i];
-            }
-            if(orr == maxi){
-                ans++;
-            }
-            return;
-        }
-        backtrack(v,nums,idx+1);
-        v[idx] = true;
-        backtrack(v,nums,idx+1);
-        v[idx] = false;
-        return;
-    }
-    
     int countMaxOrSubsets(vector<int>& nums) {
-        maxi = 0 , ans = 0;
+        int maxi = 0, ans = 0, n = nums.size();
         for(auto num:nums) maxi |= num;
-        int n = nums.size();
-        vector<bool>v(n,false);
-        backtrack(v,nums,0);
+        for(int mask = 1; mask<(1<<n);mask++){
+            int orr = 0;
+            for(int i=0;i<n;i++){
+                if((mask>>i) & 1){
+                    orr |= nums[i];
+                }
+            }
+            if(orr == maxi) ans++;
+        }
         return ans;
     }
 };
