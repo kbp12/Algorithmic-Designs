caf5007bfe97534eccc838843b61f247781a4717
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-10-29 19:18:20 +0530

    Time: 1513 ms (5.02%), Space: 202.7 MB (5.82%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.cpp
@@ -5,18 +5,17 @@ public:
         vector<vector<int>>v(n,vector<int>(2));
         for(int i=0;i<n;i++){
             v[i][0] = growTime[i];
-            v[i][1] = i;
+            v[i][1] = plantTime[i];
         }
         sort(v.begin(),v.end(),[](vector<int>&a,vector<int>&b){
-            if(a[0]>b[0]) return true;
-            if(a[0]<b[0]) return false;
-            return a[1]<b[1];
+            if(a[0]!=b[0]) return a[0]>b[0];
+            return a[1]>b[1];
         });
         
-        int ans = INT_MIN;
+        int ans = 0;
         int till = 0;
         for(int i=0;i<n;i++){
-            till += plantTime[v[i][1]];
+            till += v[i][1];
             ans = max(ans,till+v[i][0]);
         }
         
