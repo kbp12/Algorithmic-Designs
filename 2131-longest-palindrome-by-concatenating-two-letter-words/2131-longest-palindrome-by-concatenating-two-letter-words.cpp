450370685eac51d219f5c464ec9e61ee8dfcb36e
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-11-03 18:46:16 +0530

    Time: 721 ms (60.39%), Space: 167.9 MB (81.81%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         int n = words.size();
-        map<string,int>m;
+        unordered_map<string,int>m;
         int mid = 0;
         for(int i=0;i<n;i++){
             m[words[i]]++;
