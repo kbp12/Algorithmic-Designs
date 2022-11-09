92cf9951570ee2f376d31edfa5ea0ea05f0850b9
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-11-09 06:49:28 +0530

    Time: 2405 ms (5.53%), Space: 181.6 MB (12.68%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int countPairs(vector<int>& deliciousness) {
+        vector<int>pow2;
+        pow2.push_back(1);
+        int temp = 2;
+        for(int i=1;i<=21;i++){
+            pow2.push_back(temp);
+            temp*=2;
+        }
+        int ans = 0, m = pow2.size(), mod = 1e9+7;;
         unordered_map<int,int>umap;
-        int ans = 0, mod = 1e9+7;
-        for(auto d:deliciousness){
-            for(int pow2=1;pow2<(1<<22);pow2*=2){
-                ans = (ans + umap[pow2 - d])%mod;
+        for(int i=0;i<deliciousness.size();i++){
+            for(int j=0;j<m;j++){
+                ans=( ans + umap[pow2[j] - deliciousness[i]])%mod;
             }
-            umap[d]++;
+            umap[deliciousness[i]]++;
         }
         return ans;
     }
