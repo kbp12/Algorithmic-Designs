f5c9d9edc27bb94a355e0a8d0e0ae2cd3380afa6
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-11-09 06:48:37 +0530

    Time: 2105 ms (12.51%), Space: 181.4 MB (15.26%) - LeetHub

--- 1711-count-good-meals/1711-count-good-meals.cpp
+++ 1711-count-good-meals/1711-count-good-meals.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int,int>umap;
         int ans = 0, mod = 1e9+7;
         for(auto d:deliciousness){
-            for(int pow2=1;pow2<=(1<<22);pow2*=2){
+            for(int pow2=1;pow2<(1<<22);pow2*=2){
                 ans = (ans + umap[pow2 - d])%mod;
             }
             umap[d]++;
