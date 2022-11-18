430d27e1514b693b20ea529f641131a5c6482542
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-11-18 12:38:40 +0530

    Time: 618 ms (10.34%), Space: 87.5 MB (30.67%) - LeetHub

--- 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
+++ 2289-steps-to-make-array-non-decreasing/2289-steps-to-make-array-non-decreasing.cpp
@@ -2,17 +2,19 @@ class Solution {
 public:
     int totalSteps(vector<int>& nums) {
         int n = nums.size();
-        stack<int>st;
-        vector<int>dp(n,0);
-        int ans = 0;
+        stack<int>mono; // store elements in increasing order
+
+        int max_count = 0;
+        vector<int>dp(n , 0);
         for(int i=n-1;i>=0;i--){
-            while(st.size() && nums[i]>nums[st.top()]){
-                dp[i] = max(dp[i]+1,dp[st.top()]);
-                st.pop();
-                ans = max(ans,dp[i]);
+            while(mono.size() and nums[mono.top()] < nums[i]){
+                int j = mono.top();
+                dp[i] = max(dp[i] + 1 , dp[j]);
+                mono.pop();
             }
-            st.push(i);
+            max_count = max(max_count , dp[i]);
+            mono.push(i);
         }
-        return ans;
+        return max_count;
     }
 };
