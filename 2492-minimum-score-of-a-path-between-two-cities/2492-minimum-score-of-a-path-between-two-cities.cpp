ca61586a7ea72f40544d6e5fb18e63d0fd9620d6
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-12-05 12:47:41 +0530

    Time: 2919 ms (9.52%), Space: 209.3 MB (9.52%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
-    int dfs(vector<vector<pair<int,int>>>& adj,  set<pair<int,int>>& s, int node){
+    set<pair<int,int>>s;
+    int dfs(vector<vector<pair<int,int>>>& adj, int node){
         int dist = INT_MAX;
         for(auto next:adj[node]){
             if(s.find({node , next.first}) == s.end() and s.find({next.first , node}) == s.end()){
                 int val = next.second;
                 s.insert({node , next.first});
-                int d = dfs(adj,s,next.first);
+                int d = dfs(adj,next.first);
                 dist = min({dist , d , val});
             }
         }
@@ -19,7 +20,7 @@ public:
             adj[road[0]].push_back({road[1] , road[2]});
             adj[road[1]].push_back({road[0] , road[2]});
         }
-        set<pair<int,int>>s;
-        return dfs(adj,s,1);
+        
+        return dfs(adj,1);
     }
 };
