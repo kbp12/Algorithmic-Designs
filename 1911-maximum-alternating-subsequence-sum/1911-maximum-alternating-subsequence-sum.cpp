35558ebba4000b5c3af657abe01042cb45ba4371
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2022-12-18 09:57:40 +0530

    Time: 467 ms (53.77%), Space: 153.2 MB (25.46%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -9,7 +9,7 @@ public:
             // adding this element into the sequence , not adding this element into the sequence
             dp[i][0] = max(dp[i-1][0] , dp[i-1][1] - nums[i]);
             dp[i][1] = max(dp[i-1][1] , dp[i-1][0] + nums[i]);
-            ans = max({ans , dp[i][0] , dp[i][1]});
+            ans = max(ans , dp[i][1]);
         }
         return ans;
     }
