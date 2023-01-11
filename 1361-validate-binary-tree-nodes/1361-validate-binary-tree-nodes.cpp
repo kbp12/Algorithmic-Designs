fb0d3a3531e5dd5d4492cf90f0671f2f05e53acc
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2023-01-11 20:07:23 +0530

    Time: 47 ms (86.79%), Space: 32.2 MB (85.23%) - LeetHub

--- 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
+++ 1361-validate-binary-tree-nodes/1361-validate-binary-tree-nodes.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    // Idea: A valid BT has each node with exactly one parent and exactly one node with no parent (root)
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
-        // map to keep count of no. of parent for each node
         vector<int> parentFreq(n);
         for (int i = 0; i < n; i++)
             if ((leftChild[i] >= 0 and ++parentFreq[leftChild[i]] > 1) or (rightChild[i] >= 0 and ++parentFreq[rightChild[i]] > 1))
                 return false;
-        // find root (if more than 1 root exist then return false)
+       
         int root = -1;
         for (int i = 0; i < n; i++)
             if (!parentFreq[i]) {
@@ -15,8 +13,6 @@ public:
                     root = i;
                 else return false;
             }
-        // last IMP check: apply dfs on root to calculate total nodes in tree, it should be n
-        // this check helps in handling the testcases: 4, [1,0,3,-1], [-1,-1,-1,-1]
         return countNodes(leftChild, rightChild, root) == n;
     }
     
