2153797d86d55b3df99394d85b14788c1897ec69
Kedar Badrinath Pathade <71080836+kbp12@users.noreply.github.com>
2023-01-29 19:28:07 +0530

    Time: 21 ms (62.50%), Space: 5.9 MB (87.50%) - LeetHub

---
+++ 2550-count-collisions-of-monkeys-on-a-polygon/2550-count-collisions-of-monkeys-on-a-polygon.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int monkeyMove(int n) {
+        int mod = 1e9+7;
+        long long ans = 1;
+        int k = n/100005;
+        int m = n%100005;
+        if(k>0){
+            for(int i=1;i<=100005;i++){
+                ans = (ans*2)%mod;
+            }
+        }
+        long long res = ans;
+        for(int i=2;i<=k;i++){
+            res = (res*ans)%mod;
+        }
+        for(int i=1;i<=m;i++){
+            res = (res*2)%mod;
+        }
+        return (res+mod-2)%mod;
+    }
+};
